238a65ca3ccd6520720b58760eb899e3a8f6c8db
Jacob Misirian <misirianrule@gmail.com>
2017-03-11 11:39:08 -0700

    Added help commands

---
+++ src/Mothership/TelnetServer/ClientCommands/HelpCommand.cs
@@ -0,0 +1,19 @@
+﻿using Mothership.Networking;
+
+namespace Mothership.TelnetServer.ClientCommands
+{
+    public class HelpCommand : IClientCommand
+    {
+        public string Name { get { return "help"; } }
+        public string Syntax { get { return "help"; } }
+
+        public void Invoke(TelnetServer server, TcpClient user, TelnetSession session, params string[] args)
+        {
+            user.WriteLine("Available Client Commands");
+            user.WriteLine("#########################");
+            foreach (var command in server.ClientCommands.Values)
+                user.WriteLineCentered("#", command.Syntax, "#", 25);
+            user.WriteLine("#########################");
+        }
+    }
+}
---
+++ src/Mothership/TelnetServer/ServerCommands/HelpCommand.cs
@@ -0,0 +1,19 @@
+﻿using Mothership.Networking;
+
+namespace Mothership.TelnetServer.ServerCommands
+{
+    public class HelpCommand : IServerCommand
+    {
+        public string Name { get { return "help"; } }
+        public string Syntax { get { return "help"; } }
+
+        public void Invoke(TelnetServer server, TcpClient user, TelnetSession session, params string[] args)
+        {
+            user.WriteLine("Available Server Commands");
+            user.WriteLine("############################");
+            foreach (var command in server.ServerCommands.Values)
+                user.WriteLineCentered("#", command.Syntax, "#", 28);
+            user.WriteLine("############################");
+        }
+    }
+}
